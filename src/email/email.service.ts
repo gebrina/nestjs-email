0acba5bce37947be325d654179c8b7af042fc7ca
gebrina <gsonofmika@gmail.com>
2023-10-28 20:56:48 +0300

    added send email functionality

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,7 +1,29 @@
 import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
+import { Payment } from 'src/interfaces/payment.interface';
+import { User } from 'src/interfaces/user.interface';
 
 @Injectable()
 export class EmailService {
   constructor(private mailerService: MailerService) {}
+
+  async sendEmail(user: User, payment: Payment) {
+    const service_url = 'http://yourwebsite.com/service-category/service';
+
+    try {
+      const response = await this.mailerService.sendMail({
+        to: user.email,
+        subject: 'Payment',
+        template: './payment',
+        context: {
+          name: user.username,
+          service_url,
+          amount: payment.amount,
+        },
+      });
+      return response;
+    } catch (e) {
+      return e.message;
+    }
+  }
 }
